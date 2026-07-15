98aaa80cf49b91688ecbbf4242bc4c99ad290e47
niushamaryan <niushamaryan92@gmail.com>
2026-07-15 09:22:29 +0330

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,43 @@ item.style.transition="all .8s ease";
 observer.observe(item);
 
 });
+const words = [
+"AI Developer",
+"Web Designer",
+"Prompt Engineer",
+"Automation Builder"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typing = document.getElementById("typing");
+
+function typeEffect(){
+
+const current = words[wordIndex];
+
+if(!isDeleting){
+typing.textContent = current.substring(0,charIndex++);
+}else{
+typing.textContent = current.substring(0,charIndex--);
+}
+
+let speed = 120;
+
+if(!isDeleting && charIndex === current.length+1){
+speed = 1500;
+isDeleting = true;
+}
+
+if(isDeleting && charIndex===0){
+isDeleting=false;
+wordIndex=(wordIndex+1)%words.length;
+}
+
+setTimeout(typeEffect,speed);
+
+}
+
+typeEffect();
